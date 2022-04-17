4d816277e9fa5ea3c5b6e55b926c080c7b4e8b2a
rasedul99 <rasedul2199@gmail.com>
2022-04-18 02:34:41 +0600

    spinner added

--- src/components/RequiredAuth.js
+++ src/components/RequiredAuth.js
@@ -2,10 +2,14 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import auth from "../firebase.init.js";
+import Loading from "./Loading/Loading.js";
 
 const RequiredAuth = ({ children }) => {
   let location = useLocation();
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+    if (loading) {
+      return <Loading />;
+    }
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
